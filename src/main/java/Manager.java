622dc4466a3e9fef818cf94e9d3fdad1cf317b69
Kris <kristina_lacrima@mail.ru>
2023-08-20 12:51:13 +0300

    set limit 5

--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,6 +1,7 @@
 public class Manager {
     private String[] films = new String[0];
 
+
     public void add(String film) {
         String[] tmp = new String[films.length + 1];
         for (int i = 0; i < films.length; i++) {
@@ -15,6 +16,20 @@ public class Manager {
         return films;
     }
 
+
+    public String[] findLast() {
+        int resultLength;
+        if (films.length < 5) {
+            resultLength = films.length;
+        } else {
+            resultLength = 5;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
 
     public String[] findLast(int limit) {
         int resultLength;
